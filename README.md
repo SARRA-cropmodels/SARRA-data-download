90aee11f30f2b11f8bb10853f10fbb6056f0bf24
Jérémy Lavarenne <jeremy.lavarenne@cirad.fr>
2023-03-30 15:38:52 +0200

    updated notebook and source to get AgERA5 data

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 
 # SARRA data download
+
 This repo is a collection of tools to download and prepare climate and weather files necessary for the use of [SARRA-O](https://gitlab.cirad.fr/sarrao/model/sarrao) and [SARRA-Py](https://github.com/SARRA-cropmodels/SARRA-Py) spatialized crop simulation models. Its rationale is to allow for easy download of time series datasets from different data providers for Africa.
 
-## How to install
+
+
+## 1. How to install
+
 You will need Python 3.9.6 or above.
 
 Then, clone this repo and install its dependencies :
@@ -11,80 +15,33 @@ Then, clone this repo and install its dependencies :
     cd SARRA_data-download
     pip install -r requirements.txt
 
-## How to use
-**Get satellite rainfall estimates**
-
-This Jupyter notebook contains code to download and prepare daily satellite rainfall estimates for a given year from three data sources : TAMSAT (https://www.tamsat.org.uk/), CHIRPS (https://www.chc.ucsb.edu/data/chirps), and IMERG (https://gpm.nasa.gov/data/imerg).
-
-Open the notebook, modify the parameters in the appropriate section and run the cells.
 
 
+## 2. How to use
 
-**AgERA5 data download**
-First, [setup a Copernicus Climate Data Store API key](https://cds.climate.copernicus.eu/api-how-to) and accept [Copernicus Terms of Service](https://cds.climate.copernicus.eu/cdsapp/#!/terms/licence-to-use-copernicus-products).
+### 2.1. Get satellite rainfall estimates
 
-    cd SARRA_data_download
-    python get_AgERA5_data.py
+The './notebooks/get_satellite_rainfall_estimated.ipynb' notebook allows for downloading of daily rainfall estimates product from different providers, and preprocessing for their direct use in crop simulation models of the SARRA family. The output format is then a series of geotiff files, one per day, with the same spatial resolution as the input data. 
 
-This script automatically downloads AgERA5 data for the whole month of the last available date, every day at 12:00 PM. For example, if the current date is 2022-09-15, the last available date in AgERA5 will be 2022-09-07, thus, the script will download all data from 2022-09-01 to 2022-09-07.
-Download is performed by default on an extent covering all West Africa (29°N,-20°E to 3.5°N,26°E).
-Retrieved variables are Tmin, Tmax, solar radiation. 
-ET0 is computed using Hargraeves formula.
-Output files will be daily geotiffs, as required to run SARRA-O.
+Rainfall estimates products available :
+- [TAMSAT](https://www.tamsat.org.uk/), resolution 0.037°
+- [CHIRPS](https://www.chc.ucsb.edu/data/chirps), resolution 0.05°
+- [IMERG](https://gpm.nasa.gov/data/imerg), resolution 0.1°
 
-The downloaded and prepared data will be stored in the `./data/3_output/` path.
+Note : before downloding IMERG data, you must have a NASA Earthdata account. Check [this document](https://gpm.nasa.gov/sites/default/files/2021-01/arthurhouhttps_retrieval.pdf) for more information about how to create an account and get your credentials.
+
+To use, open the './notebooks/get_satellite_rainfall_estimated.ipynb' notebook in Jupyter, JupyterLab or VSCode, modify the parameters in the appropriate section and run the cells. The output files will be stored in the './data/3_output/' directory.
 
 
 
+### 2.2. AgERA5 data download
 
-This script automatically downloads TAMSAT v3.1 data for the whole month of the last AgERA5 available date, every day at 12:00 PM. For example, if the current date is 2022-09-15, the last available date in AgERA5 will be 2022-09-07, thus, the script will download all TAMSAT data from 2022-09-01 to 2022-09-07.
-Download is performed by default on an extent covering all West Africa (29°N,-20°E to 3.5°N,26°E).
-Retrieved variables is `rfe_filled`. 
-Output files will be daily geotiffs, as required to run SARRA-O.
+The './notebooks/get_AgERA5_data.ipynb' notebook allows for downloading of daily AgERA5 climate data from the [Copernicus Climate Data Store (CDS)](https://cds.climate.copernicus.eu/#!/home), and preprocessing for their direct use in crop simulation models of the SARRA family. The output format is a series of geotiff files, one per variable and per day.
 
-The downloaded and cropped data will be stored in the `./data/3_output/` path.
+The cdsapi package is used to download data from CDS. This has the advantage of magaging the caching of already passed requests, thus to speed up the downloading process.
 
-**AgERA5 point data download**
+AgERA5 data should be produced daily, with a 7-day lag, according to the [AgERA-5 frequency update](https://confluence.ecmwf.int/display/CUSF/AgERA-5+frequency+update) documentation.
 
-    cd SARRA_data_download
-    python get_AgERA5_data_point.py
+Before running the notebook for the first time, [follow these instructions](https://cds.climate.copernicus.eu/api-how-to) to setup a Copernicus Climate Data Store API key on your machine, and accept [Copernicus Terms of Service](https://cds.climate.copernicus.eu/cdsapp/#!/terms/licence-to-use-copernicus-products).
 
-This script automatically downloads AgERA5 data for a whole year, for the given coordinates.
-Retrieved variables are Tmin, Tmax, Tmoy, solar radiation. 
-ET0 is computed using Penman-Monteith as implemented in pcse.
-Elevation info needed by pcse is retrieved from OpenElevation API.
-Output files will be saved as csv, as required to run SARRA-H.
-
-**TAMSAT point data download**
-
-This script automatically downloads TAMSAT tabular data format, for the given coordinates.
-The downloaded and prepared data will be stored in the `./data/3_output/` path.
-
-# ASSETS :
-This repo also hosts a downscaled iSDAsoil soil texture class (USDA system) (https://zenodo.org/record/4094616#.Y0RBArTP1mN) to be used with SARRA. Downscaling was performed from the 0-20cm depth classification, at 4km resolution, aligned with TAMSAT raster files. Values were converted to SARRA-O soil type format, a 7 to 8 digit integer where the 6 last digits must be zeroes, and the first digits are the USDA soil code corresponding to the soil characteristics described in the `./data/csvTypeSol/` folder of SARRA-O executable file. Also, the null category value was replaced from 255 in iSDA to 0 in SARRA format.
-
-https://dataverse.cirad.fr/dataset.xhtml?persistentId=doi:10.18167/DVN1/YSVTS2
-
-This repo also hosts a downscaled ISRIC Africa SoilGrids soil texture class (USDA system) to be used with SARRA. Downscaling was performed from computing the mode of the different layers in the 0-60cm depth range of the classification, at 4km resolution, aligned with TAMSAT raster files. Values were converted to SARRA-O soil type format, a 7 to 8 digit integer where the 6 last digits must be zeroes, and the first digits are the USDA soil code corresponding to the soil characteristics described in the `./data/csvTypeSol/` folder of SARRA-O executable file. Also, the null category value was replaced from 255 in iSDA to 0 in SARRA format.
-
-These assets can be found in `./soil_maps/` path.
-To be used in SARRA-O, these files must be renamed `soil_africa_sarrah_tamsat.tif` and put into `./data/` folder of SARRA-O executable.
-
-# TO-DO :
-Calcul du ET0 par PM
-Utiliser un MNT à la résolution des données pluie
-TAMSAT resolution
-sols actuellement à 9km
-isda soil : à dégrader à la résolution de la pluie, ou a 3 km
-
-
-https://www.catds.fr/Products/Available-products-from-CEC-SM/L4-Land-research-products/PrISM-precipitation-product
-https://zenodo.org/record/5998113#.YzqpyITP02w
-https://zenodo.org/record/5998113#.Y1k1BITP02w
-https://earth.esa.int/eogateway/news/smos-data-improve-estimates-of-rainfall-in-africa
-
-https://hal.inrae.fr/hal-02626156v2/document
-https://www.mdpi.com/2072-4292/12/3/481
-https://www.mdpi.com/2072-4292/14/3/746
-
-
+To use, open the './notebooks/get_AgERA5_data.ipynb' notebook in Jupyter, JupyterLab or VSCode, modify the parameters in the appropriate section and run the cells. The output files will be stored in the './data/3_output/' directory.
---
+++ src/SARRA_data_download/get_AgERA5_data.py
@@ -0,0 +1,399 @@
+import cdsapi
+import zipfile
+import xarray as xr 
+import rioxarray as rio 
+import os
+from tqdm import tqdm
+import pandas as pd
+from datetime import datetime as dt
+import datetime
+import rasterio
+from rasterio.plot import show
+from tqdm import tqdm
+import numpy as np
+import schedule
+
+
+
+
+def download_AgERA5_data(area, selected_area, variables, mode="month", query=dt.today(), save_path="../data/"):
+    """
+    if mode "month", download all days of the month of the datetime.date passed in query
+    if mode "year", downloads all days of the year of the datetime.date passed in query    
+    by default, is in mode "month", and query is the current date
+    """
+
+    print("===== download_AgERA5_data =====")
+
+    # try:
+    if mode == "month":
+        query_year = query.year # int
+        query_month = query.strftime('%m') # str
+
+        print("Mode 'month' acknowledged. Will download data for month",query.month,"/",query.year)
+        print("Please note that last available date on AgERA5 should be",dt.today()-datetime.timedelta(days=8))
+
+    elif mode == "year":
+        query_year = query.year
+
+        print("Mode 'year' acknowledged. Will download data for year",query.year)
+        print("Please note that last available date on AgERA5 should be",(dt.today()-datetime.timedelta(days=8)).date())
+
+    else :
+        raise Exception("The mode passed ("+mode+") is incorrect. Please use either 'month' or 'year'")
+
+
+    c = cdsapi.Client()
+
+    if not os.path.exists(os.path.join(save_path,"0_downloads/")):
+        os.makedirs(os.path.join(save_path,"0_downloads/"))
+
+    for variable in variables :
+
+        request = {
+                'format': 'zip',
+                'day': [
+                    '01', '02', '03',
+                    '04', '05', '06',
+                    '07', '08', '09',
+                    '10', '11', '12',
+                    '13', '14', '15',
+                    '16', '17', '18',
+                    '19', '20', '21',
+                    '22', '23', '24',
+                    '25', '26', '27',
+                    '28', '29', '30',
+                    '31',
+                ],
+                'year': [str(query_year)],
+                'variable': variable[0],
+                'statistic': variable[1],
+                'area': area[selected_area],
+            }
+
+        if mode == "month":
+            zip_path = os.path.join(save_path,'0_downloads/AgERA5_'+selected_area+'_'+variable[0]+'_'+variable[1]+"_"+str(query_year)+'_'+query_month+'.zip')
+            request["month"] = query_month
+
+        if mode == "year": 
+            zip_path = os.path.join(save_path,'0_downloads/AgERA5_'+selected_area+'_'+variable[0]+'_'+variable[1]+"_"+str(query_year)+'.zip')
+            request["month"] = [
+                '01', '02', '03',
+                '04', '05', '06',
+                '07', '08', '09',
+                '10', '11', '12',
+            ]
+
+        # la requête doit être adaptée pour cette variable
+        if variable[0] == "solar_radiation_flux" :
+            del request["statistic"]
+
+        c.retrieve(
+            'sis-agrometeorological-indicators',
+            request,
+            zip_path)
+
+        print("Download OK")
+
+    # except :
+    #     print("/!\ Download NOT OK")
+
+
+
+
+def extract_AgERA5_data(area, selected_area, variables, mode="month", query=dt.today(), save_path="../data/"):
+    """
+    mode can be 'month' or 'year'
+    uqery must be datetime.date
+
+
+    """
+
+    print("===== extract_agERA5_month =====")
+
+    try:
+        query_year = query.year # int
+        query_month = query.strftime('%m') # str
+
+        for variable in tqdm(variables) :
+
+            if mode == "month":
+                zip_path = os.path.join(save_path,'0_downloads/AgERA5_'+selected_area+'_'+variable[0]+'_'+variable[1]+"_"+str(query_year)+'_'+query_month+'.zip')
+                
+            elif mode == "year":
+                zip_path = os.path.join(save_path,'0_downloads/AgERA5_'+selected_area+'_'+variable[0]+'_'+variable[1]+"_"+str(query_year)+'.zip')
+            
+            extraction_path = os.path.join(save_path,'1_extraction/AgERA5_'+selected_area+'/'+str(query_year)+"/"+variable[0]+'_'+variable[1]+'/')
+            
+            try:
+                with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+                    zip_ref.extractall(extraction_path)
+            except:
+                pass
+        
+        print("Extraction OK")
+
+    except :
+        print("/!\ Extraction NOT OK")
+
+
+
+
+def convert_AgERA5_netcdf_to_geotiff(area, selected_area, variables, query=dt.today(), save_path="../data/"):
+    """
+    converts netcdf to geotiff
+    must define which variable to convert, and for which year, as defined with a datetime.date
+    """
+
+    print("===== convert_AgERA5_netcdf_to_geotiff =====")
+
+    try:
+
+        query_year = query.year # int
+        query_month = query.strftime('%m') # str
+        
+        for variable in variables :
+            
+            extraction_path = os.path.join(save_path,'1_extraction/AgERA5_'+selected_area+'/'+str(query_year)+"/"+variable[0]+'_'+variable[1]+'/')
+            nc_files = os.listdir(extraction_path)
+            conversion_path = os.path.join(save_path,'2_conversion/AgERA5_'+selected_area+'/'+variable[0]+'_'+variable[1]+'/')
+
+            if not os.path.exists(conversion_path):
+                os.makedirs(conversion_path)
+
+            for nc_file in tqdm(nc_files) :
+                # d'après https://help.marine.copernicus.eu/en/articles/5029956-how-to-convert-netcdf-to-geotiff
+                nc_file_content = xr.open_dataset(os.path.join(extraction_path, nc_file))
+                xarray_variable_name = list(nc_file_content.keys())[0]
+                bT = nc_file_content[xarray_variable_name]
+                bT = bT.rio.set_spatial_dims(x_dim='lon', y_dim='lat')
+                bT.rio.crs
+                bT.rio.write_crs("epsg:4326", inplace=True)
+
+                filename = variable[0]+"_"+variable[1]+"_"+pd.to_datetime(nc_file_content.time.values[0]).strftime('%Y')+"_"+pd.to_datetime(nc_file_content.time.values[0]).strftime('%m')+"_"+pd.to_datetime(nc_file_content.time.values[0]).strftime('%d')+".tif"
+                bT.rio.to_raster(os.path.join(conversion_path, filename))
+
+
+    except :
+        print("/!\ Conversion to GeoTIFFs NOT OK")
+
+
+
+
+def calculate_AgERA5_ET0_and_save(area, selected_area, variables, query=dt.today(), save_path="../data/"):
+
+    print("===== calculate_AgERA5_ET0_and_save =====")
+
+    try:
+
+        query_year = query.year
+        query_month = query.strftime('%m')
+
+        # tmin
+        variable = variables[0]
+        conversion_path_tmin = os.path.join(save_path,'2_conversion/AgERA5_'+selected_area+"/"+variable[0]+'_'+variable[1]+'/')
+        list_files_tmin = os.listdir(conversion_path_tmin)
+
+        # tmax
+        variable = variables[1]
+        conversion_path_tmax = os.path.join(save_path,'2_conversion/AgERA5_'+selected_area+"/"+variable[0]+'_'+variable[1]+'/')
+        list_files_tmax = os.listdir(conversion_path_tmax)
+
+        # irrad
+        variable = variables[2]
+        conversion_path_irrad = os.path.join(save_path,'2_conversion/AgERA5_'+selected_area+"/"+variable[0]+'_'+variable[1]+'/')
+        list_files_irrad = os.listdir(conversion_path_irrad)
+
+        # vapour pressure
+        variable = variables[3]
+        conversion_path_vp = os.path.join(save_path,'2_conversion/AgERA5_'+selected_area+"/"+variable[0]+'_'+variable[1]+'/')
+        list_files_vp = os.listdir(conversion_path_vp)
+
+        # wind
+        variable = variables[4]
+        conversion_path_wind = os.path.join(save_path,'2_conversion/AgERA5_'+selected_area+"/"+variable[0]+'_'+variable[1]+'/')
+        list_files_wind = os.listdir(conversion_path_wind)
+
+        # tmean
+        variable = variables[5]
+        conversion_path_tmean = os.path.join(save_path,'2_conversion/AgERA5_'+selected_area+"/"+variable[0]+'_'+variable[1]+'/')
+        list_files_tmean = os.listdir(conversion_path_tmean)
+
+
+
+        # testing if all folders have the same number of files
+        len(list_files_tmin) == len(list_files_tmax) == len(list_files_irrad) == len(list_files_vp) == len(list_files_wind) == len(list_files_tmean)
+
+
+
+
+        for i in tqdm(range(len(list_files_irrad))):
+
+            ## on charge les arrays
+
+            ######################## tmin
+            img_tmin = rasterio.open(os.path.join(conversion_path_tmin,list_files_tmin[i]))
+            arr_tmin = img_tmin.read()
+            arr_tmin = arr_tmin - 273.15
+
+            geotiff_path = os.path.join(save_path,'3_output/AgERA5_'+selected_area+"/2m_temperature_24_hour_minimum/")
+            
+
+            if not os.path.exists(geotiff_path):
+                os.makedirs(geotiff_path)
+
+            new_dataset = rasterio.open(
+                geotiff_path+"2m_temperature_24_hour_minimum_"+'_'.join(list_files_tmin[i].split("_")[-3:]),
+                'w',
+                driver='GTiff',
+                height=arr_tmin.shape[1],
+                width=arr_tmin.shape[2],
+                count=1,
+                dtype=arr_tmin.dtype,
+                crs=img_tmin.crs,
+                transform=img_tmin.transform,
+            )
+
+            new_dataset.write(arr_tmin[0,:,:], 1)
+            
+
+            ######################## tmax
+            img_tmax = rasterio.open(os.path.join(conversion_path_tmax,list_files_tmax[i]))
+            arr_tmax = img_tmax.read()
+            arr_tmax = arr_tmax - 273.15
+
+            geotiff_path = os.path.join(save_path,'3_output/AgERA5_'+selected_area+"/2m_temperature_24_hour_maximum/")
+
+            if not os.path.exists(geotiff_path):
+                os.makedirs(geotiff_path)
+
+            new_dataset = rasterio.open(
+                geotiff_path+"2m_temperature_24_hour_maximum_"+'_'.join(list_files_tmax[i].split("_")[-3:]),
+                'w',
+                driver='GTiff',
+                height=arr_tmax.shape[1],
+                width=arr_tmax.shape[2],
+                count=1,
+                dtype=arr_tmax.dtype,
+                crs=img_tmax.crs,
+                transform=img_tmax.transform,
+            )
+
+            new_dataset.write(arr_tmax[0,:,:], 1)
+
+            ######################## irrad
+            # J/m²/d
+            img_irrad = rasterio.open(os.path.join(conversion_path_irrad,list_files_irrad[i]))
+            arr_irrad = img_irrad.read()
+            # pcse needs J/m²/d, no conversion needed
+            # however SARRA needs kJ/m²/d
+            arr_irrad = np.round(arr_irrad / 1000,0) # .astype(int)
+
+            # depending on the version, it may need MJ/m²/j
+            # arr_irrad = np.round(arr_irrad / 1000000,0)
+
+            geotiff_path = os.path.join(save_path,'3_output/AgERA5_'+selected_area+"/solar_radiation_flux_daily/")
+
+            if not os.path.exists(geotiff_path):
+                os.makedirs(geotiff_path)
+
+            new_dataset = rasterio.open(
+                geotiff_path+"solar_radiation_flux_daily_"+'_'.join(list_files_irrad[i].split("_")[-3:]),
+                'w',
+                driver='GTiff',
+                height=arr_irrad.shape[1],
+                width=arr_irrad.shape[2],
+                count=1,
+                dtype=arr_irrad.dtype,
+                crs=img_irrad.crs,
+                transform=img_irrad.transform,
+            )
+
+            new_dataset.write(arr_irrad[0,:,:], 1)
+
+
+            # hPa
+            img_vp = rasterio.open(os.path.join(conversion_path_vp,list_files_vp[i]))
+            arr_vp = img_vp.read()
+            # pcse needs hPa, no conversion needed
+
+            # m/s
+            img_wind = rasterio.open(os.path.join(conversion_path_wind,list_files_wind[i]))
+            arr_wind = img_wind.read()
+            # pcse needs m/s, no conversion needed
+
+            img_tmean = rasterio.open(os.path.join(conversion_path_tmean,list_files_tmean[i]))
+            arr_tmean = img_tmean.read()
+            arr_tmean = arr_tmean - 273.15
+
+            geotiff_path = os.path.join(save_path,'3_output/AgERA5_'+selected_area+"/2m_temperature_24_hour_mean/")
+
+            if not os.path.exists(geotiff_path):
+                os.makedirs(geotiff_path)
+
+            new_dataset = rasterio.open(
+                geotiff_path+"2m_temperature_24_hour_mean_"+'_'.join(list_files_tmean[i].split("_")[-3:]),
+                'w',
+                driver='GTiff',
+                height=arr_tmean.shape[1],
+                width=arr_tmean.shape[2],
+                count=1,
+                dtype=arr_tmean.dtype,
+                crs=img_tmean.crs,
+                transform=img_tmean.transform,
+            )
+
+            new_dataset.write(arr_tmean[0,:,:], 1)
+
+            ## on calcule le ET0
+
+            # "When solar radiation data, relative humidity data and/or wind speed data are missing,
+            # ETo can be estimated using the Hargreaves ETo equation" in FAO 56
+
+            coeff = 0.0023
+            arr_ET0 = coeff * (arr_tmean + 17.8) * 0.408 * arr_irrad/1000 * (abs(arr_tmax - arr_tmin))**0.5
+
+            ## on sauvegarde les geotiffs
+
+            geotiff_path = os.path.join(save_path,'3_output/AgERA5_'+selected_area+"/ET0Hargeaves/")
+
+            if not os.path.exists(geotiff_path):
+                os.makedirs(geotiff_path)
+            
+            # on utilise tmean pour récupérer la date dans le nom de fichier, le crs et le transform
+            new_dataset = rasterio.open(
+                geotiff_path+"ET0Hargreaves_"+'_'.join(list_files_tmean[i].split("_")[-3:]),
+                'w',
+                driver='GTiff',
+                height=arr_ET0.shape[1],
+                width=arr_ET0.shape[2],
+                count=1,
+                dtype=arr_ET0.dtype,
+                crs=img_tmean.crs,
+                transform=img_tmean.transform,
+            )
+
+            new_dataset.write(arr_ET0[0,:,:], 1)
+
+    except:
+        print("/!\ Calculation of ET0 NOT OK")
+
+
+
+
+variables = [
+    ("2m_temperature","24_hour_minimum"),
+    ("2m_temperature","24_hour_maximum"),
+    ("solar_radiation_flux", "daily"),
+    ("vapour_pressure", "24_hour_mean"),
+    ("10m_wind_speed", "24_hour_mean"),
+    ("2m_temperature","24_hour_mean"),
+]
+
+def download_AgERA5_year(query_year, area, selected_area, save_path):
+    query_date = datetime.date(query_year,1,1)
+    download_AgERA5_data(area, selected_area, variables, mode="year", query=query_date)
+    extract_AgERA5_data(area, selected_area, variables, mode="year", query=query_date)
+    convert_AgERA5_netcdf_to_geotiff(area, selected_area, variables, query=query_date) 
+    calculate_AgERA5_ET0_and_save(area, selected_area, variables, query=query_date)
+    print("===== Query date",query_date,"all done ! =====")
